d078e443b01f33ce72a59c051e305e569e7013e8
maxxlt <din.chan@mavs.uta.edu>
2021-01-07 18:26:40 -0600

    Created DTO objects

---
+++ dto/src/main/java/com/sennovate/dto/ClientDTO.java
@@ -0,0 +1,9 @@
+package com.sennovate.dto;
+
+import lombok.Data;
+
+@Data
+public class ClientDTO {
+    private long id;
+    private String fname, lname, email;
+}
---
+++ dto/src/main/java/com/sennovate/dto/PartnerDTO.java
@@ -0,0 +1,9 @@
+package com.sennovate.dto;
+
+import lombok.Data;
+
+@Data
+public class PartnerDTO {
+    private long id;
+    private String fname, lname, email;
+}
